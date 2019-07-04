fcf3d40fe7b8db5aa98793d951161177c1a346f7
guisfanton <guisfanton@hotmail.com>
2019-07-04 17:39:38 -0300

    Added Files

---
+++ src/Controlador.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Controlador {
+
+    @FXML
+    private Button b1;
+
+
+    @FXML
+    public void click(ActionEvent actionEvent) {
+        System.out.println("Cliquei");
+    }
+}
