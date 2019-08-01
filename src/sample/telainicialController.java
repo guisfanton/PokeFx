46a05dbd87a19574679914c129ae4fb41d7d03c6
guisfanton <guisfanton@hotmail.com>
2019-08-01 16:52:19 -0300

    Added files

--- src/sample/telainicialController.java
+++ src/sample/telainicialController.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
 public class telainicialController {
